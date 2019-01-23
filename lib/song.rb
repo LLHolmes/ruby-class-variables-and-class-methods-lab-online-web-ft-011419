c3e3aa2d92f9d7ca2c06e32eed107c30c71f13f3
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-23 13:33:17 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  
+  def initialize(title, artist, genre)
+    @name = title
+    @artist = artist
+    @genre =  genre
+  end
+    
+end
